ed4f629d6c24d2f3fd49ecd6b982f77d1021f745
Habzy Huang <habzyhs@gmail.com>
2013-07-05 17:46:29 +0800

    Add operation with weixin
    * Open weixin.
    * Send text messge with weixin.
    * But need waiting for the application examining and verifing.
    * So it can not work at these time.

---
+++ src/com/habzy/pillow/AppRegister.java
@@ -0,0 +1,20 @@
+/**
+ * The class copy from Weixin SDK demo
+ */
+package com.habzy.pillow;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+public class AppRegister extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final IWXAPI api = WXAPIFactory.createWXAPI(context, null);
+        api.registerApp(MainActivity.APP_ID);
+    }
+}
--- src/com/habzy/pillow/MainActivity.java
+++ src/com/habzy/pillow/MainActivity.java
@@ -13,28 +13,39 @@ import android.widget.Button;
 import android.app.Activity;
 
 import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.SendMessageToWX;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXMediaMessage;
+import com.tencent.mm.sdk.openapi.WXTextObject;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    private static final String APP_ID = "wx4de4ec7895ae07ca";
+    public static final String APP_ID = "wx4de4ec7895ae07ca";
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    
+
     private Button mShareButton;
 
+    private Button mOpenButton;
+
     private IWXAPI api;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         api = WXAPIFactory.createWXAPI(this, APP_ID, true);
         api.registerApp(APP_ID);
-        
-        mShareButton = (Button)findViewById(R.id.share);
+
+        mShareButton = (Button) findViewById(R.id.share);
         mShareButton.setOnClickListener(this);
+
+        mOpenButton = (Button) findViewById(R.id.open);
+        mOpenButton.setOnClickListener(this);
     }
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
      * @see android.view.View.OnClickListener#onClick(android.view.View)
      */
     @Override
@@ -42,12 +53,47 @@ public class MainActivity extends Activity implements OnClickListener {
         switch (v.getId()) {
             case R.id.share:
                 Log.d(TAG, "Click share button");
+                sendToWeiXin();
                 break;
-
+            case R.id.open:
+                Log.d(TAG, "Click open button: opened:" + api.openWXApp());
+                sendToWeiXin();
+                break;
             default:
                 break;
         }
-        
+
+    }
+
+    private void sendToWeiXin() {
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        creatTextMsg(req);
+        boolean result = api.sendReq(req);
+        Log.d(TAG, "Is sended:" + result);
+    }
+
+    /**
+     * Create Message type is text.
+     */
+    private void creatTextMsg(SendMessageToWX.Req req) {
+        String text = "Test from Pillow";
+        WXTextObject textObj = new WXTextObject();
+        textObj.text = text;
+
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = textObj;
+        // msg.title = "Will be ignored";
+        msg.description = text;
+
+        req.transaction = buildTransaction("text");
+        req.message = msg;
+        // Default is WXSceneSession.
+        // req.scene = SendMessageToWX.Req.WXSceneSession;
+    }
+
+    private String buildTransaction(final String type) {
+        return (type == null) ? String.valueOf(System.currentTimeMillis()) : type
+                + System.currentTimeMillis();
     }
 
 }
